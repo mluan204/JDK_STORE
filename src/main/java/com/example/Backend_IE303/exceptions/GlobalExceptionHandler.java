387a9b7c9dbaa3124910a9f97a37bb67ca8cfa1f
NgPhuong <phuongngusee@gmail.com>
2025-04-24 21:07:26 +0700

    feat: update Employee & Customer

--- src/main/java/com/example/Backend_IE303/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/Backend_IE303/exceptions/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = RuntimeException.class)
-    public ResponseEntity<String> handlingRuntimeException(RuntimeException exception){
-        return ResponseEntity.badRequest().body(exception.getMessage());
+    public ResponseEntity<ApiError> handlingRuntimeException(RuntimeException exception){
+        return ResponseEntity.ok().body(new ApiError(100,exception.getMessage()));
     }
 }
