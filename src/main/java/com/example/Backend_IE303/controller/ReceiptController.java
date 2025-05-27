afb54662f0517fb9f090e223030a02b6995c5f8a
CaoKiet251 <caoquockiet04@gmail.com>
2025-05-27 19:25:48 +0700

    fix: api receipt

--- src/main/java/com/example/Backend_IE303/controller/ReceiptController.java
+++ src/main/java/com/example/Backend_IE303/controller/ReceiptController.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 @RestController
@@ -67,5 +70,34 @@ public class ReceiptController {
                     .body("Receipt with id " + id + " not found.");
         }
     }
+
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<ReceiptDetailResponseDTO>> getFilteredReceipts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String fromDate,     // dạng: 01-01-2025
+            @RequestParam(required = false) String toDate,       // dạng: 31-01-2025
+            @RequestParam(required = false) String employeeName
+    ) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate from = null;
+        LocalDate to = null;
+
+        try {
+            if (fromDate != null && !fromDate.isEmpty()) {
+                from = LocalDate.parse(fromDate, formatter);
+            }
+            if (toDate != null && !toDate.isEmpty()) {
+                to = LocalDate.parse(toDate, formatter);
+            }
+        } catch (DateTimeParseException e) {
+            return ResponseEntity.badRequest().body(Page.empty()); // hoặc trả lỗi cụ thể
+        }
+
+        Page<ReceiptDetailResponseDTO> result = receiptService.getReceiptsPaginatedFiltered(page, size, from, to, employeeName);
+        return ResponseEntity.ok(result);
+    }
+
 
 }
--- src/main/java/com/example/Backend_IE303/repository/ReceiptRepository.java
+++ src/main/java/com/example/Backend_IE303/repository/ReceiptRepository.java
@@ -1,8 +1,12 @@
 package com.example.Backend_IE303.repository;
 
 import com.example.Backend_IE303.entity.Receipt;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReceiptRepository extends JpaRepository<Receipt, Integer> {
 
+    Page<Receipt> findAll(Specification<Receipt> spec, Pageable pageable);
 }
